a3afa513567d2055f920b2cac74bfddc22978e95
Diego Figueirêdo <8kball@gmail.com>
2022-02-15 18:22:54 -0300

    chore: create homepage form

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,26 +1,66 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+import styled from 'styled-components';
 
-const Form = () => {
-  const [username, setUsername] = useState('');
+const Form = styled.form`
+  margin: 100px auto;
+  gap: 10px;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #eee;
+  padding: 20px;
+  border-radius: 4px;
+  max-width: 300px;
 
-  const handleChange = (e) => {
-    const { target: { value } } = e;
-    setUsername(value);
+  & svg {
+    align-self: center;
+    color: #ddd;
+    font-size: 6rem;
+    margin-bottom: 20px;
+  }
+
+  &:focus {
+    background: red;
+  }
+`;
+
+const Start = styled.button`
+  border: none;
+  background-color: royalblue;
+  border-radius: 4px;
+  padding: 8px 0;
+  color: white;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #315cdf;
+  }
+`;
+
+const Input = styled.input`
+  border: 1px solid #bbb;
+  padding: 6px;
+  border-radius: 4px;
+`;
+
+const Home = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { username } = e.target;
+    
+    if (!username.length) return;
+
+    const user = username.value;
+    localStorage.setItem('user', user);
   };
 
   return (
-    <form>
-      <label htmlFor="username">
-        User:
-        <input
-          value={ username }
-          onChange={ handleChange }
-          type="text"
-          id="username"
-        />
-      </label>
-    </form>
+    <Form onSubmit={ handleSubmit }>
+      <FaUserCircle />
+      <Input autoFocus type="text" id="username" />
+      <Start>Começar!</Start>
+    </Form>
   );
 };
 
-export default Form;
+export default Home;
